b1fd0706a8e19f759750bdb400d9749c084f4daf
Song Sun <sunsongxp@gmail.com>
2020-08-28 18:44:30 +0800

    Update couchbase to v3

--- main.py
+++ main.py
@@ -2,19 +2,31 @@
 # -*- coding: utf-8 -*-
 import copy
 import uuid
-from couchbase.cluster import Cluster
-from couchbase.cluster import PasswordAuthenticator
+from couchbase.cluster import Cluster, ClusterOptions, QueryOptions
+from couchbase_core.cluster import PasswordAuthenticator
 
 
+# The following are deprecated note
+# ===================================================================
 # You should be able to connect to
 # the ips of the whole cluster from the maching running the script.
 # Or you'll see error like
-
+#
 # *** couchbase.exceptions._TimeoutError_0x17 (generated, catch TimeoutError): <Key='12', RC=0x17[Client-Side timeout exceeded for operation. Inspect network conditions or increase the timeout], Operational Error, Results=1, C Source=(src/multiresult.c,316), Tracing Output={"12": {"s": "kv:Unknown", "i": 823564440, "b": "bucket_sample", "r": "172.42.92.71:11210", "t": 2500000}}>
-cluster = Cluster('couchbase://couchbase_ip')
-authenticator = PasswordAuthenticator('Administrator', 'password')
-cluster.authenticate(authenticator)
-cb = cluster.open_bucket('bucket_sample')
+#
+# ===================================================================
+
+# For couchbase version 3
+cluster = Cluster(
+    'couchbase://couchbase_ip',
+    ClusterOptions(
+        PasswordAuthenticator('Administrator', 'password')
+    )
+)
+
+bucket = cluster.bucket('bucket_sample')
+coll = bucket.default_collection()
+
 
 def main():
     document = {
@@ -22,7 +34,7 @@ def main():
     }
 
     key = str(uuid.uuid4())
-    cb.upsert(key, document)
+    coll.upsert(key, document)
     print(key)
 
 
